5821f6e48b395e340852882cacf15033973414f6
EduardoArratia <eduardo.arratia.r@gmail.com>
2023-03-24 17:30:46 -0300

    css incorporado

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 // todos estos import son necesarios para la inicializacón de la página
 import express from "express";
+
+//Configura el directorio con la URL donde se va a ver.
+import { dirname,join } from "path";
+import { fileURLToPath } from "url";  
+//configura hbs
 import hbs from "hbs";
-import { fileURLToPath } from "url"; // 
-import path, { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+//importar rutas desde routes.js en carpeta routes
+import indexRoutes from "./routes/routes.js";
+import bodyParser from "body-parser"
 
+//Asignar express a una constante
 const app = express();
-const port = 3000;
 
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'), function (err) {})
+app.use(bodyParser.json()); //para soportar JSON encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true}));
+
+// __dirname es donde quedará guardado de forma dínamica el path de los archivos en caso de mover el proyecto a otra carpeta o dirección
+const __dirname = dirname(fileURLToPath(import.meta.url));
+hbs.registerPartials(join(__dirname, '/views/partials'));
+//Configurar que express use las rutas del archivo routes.js
+app.use(indexRoutes)
+////Configuración de carpeta donde están los archivos
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views','./views');
+app.use(express.static("public"));
 
-app.use('/', (req,res) => { // para usar routes, en vez de app usar routes, LEER SOBRE MODULES DE EXPRESS Y LOS MIDDLE WORD
-    res.render('index');
+
+//Levanta el servidor: hace que se vea la pagina y escuche este codigo hacia el server 3000
+app.listen(3000,function (){
+    console.log("Server cargado")
 })
-
-app.listen(port, () => {
-    console.log(`Servidor iniciado en el puerto ${port}`);
-});
--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,10 @@
 import { Router } from "express";
 const router = Router();
+
+
+
+router.get ('/', (req,res) => { // al usar routes, los get ya no son app.use sino router.get LEER SOBRE MODULES DE EXPRESS Y LOS MIDDLE WORD
+    res.render('index');
+})
+
+export default router //exportar todo lo que está en routes.js

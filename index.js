f4d43bdf4612b6e2e263229ba3da9e6c0d748eaf
EduardoArratia <eduardo.arratia.r@gmail.com>
2023-03-29 20:09:36 -0300

    cambios pagina inicio

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 // todos estos import son necesarios para la inicializacón de la página
 import express from "express";
 
+
+
 //Configura el directorio con la URL donde se va a ver.
-import { dirname,join } from "path";
-import { fileURLToPath } from "url";  
-//configura hbs
-import hbs from "hbs";
+
+import {dirname, join} from "path";
+import { fileURLToPath } from "url";
+
 
 //importar rutas desde routes.js en carpeta routes
-import indexRoutes from "./routes/routes.js";
-import bodyParser from "body-parser"
+import indexRoutes from "./routes/routes.js"
 
 //Asignar express a una constante
 const app = express();
 
-app.use(bodyParser.json()); //para soportar JSON encoded bodies
-app.use(bodyParser.urlencoded({
-    extended: true}));
-
 // __dirname es donde quedará guardado de forma dínamica el path de los archivos en caso de mover el proyecto a otra carpeta o dirección
 const __dirname = dirname(fileURLToPath(import.meta.url));
-hbs.registerPartials(join(__dirname, '/views/partials'));
+
+//Donde va la carpeta public donde irán css
+app.use(express.static(join(__dirname,"/public")))
+
+//Donde va la carpeta public donde irán los script
+app.use(express.static(join(__dirname,"/js")))
+
+//Configuracion HandleBars //
+import hbs from "hbs";
+hbs.registerPartials(__dirname + '/views/partials');
+app.set("views", join(__dirname, "views"))
+// app.set("views", join(__dirname, "views"))
+app.set('view engine', 'hbs');
+
+
 //Configurar que express use las rutas del archivo routes.js
 app.use(indexRoutes)
-////Configuración de carpeta donde están los archivos
-app.set('view engine', 'hbs');
-app.set('views','./views');
-app.use(express.static("public"));
-
 
 //Levanta el servidor: hace que se vea la pagina y escuche este codigo hacia el server 3000
 app.listen(3000,function (){
     console.log("Server cargado")
 })
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,105 @@
 import { Router } from "express";
+import bodyParser from "body-parser";
+import passport from "passport";
+import PassportLocal from "passport-local"
+import session from "express-session"
+import cookieParser from "cookie-parser";
+import fs from "fs"
+import express from "express";
+
+
 const router = Router();
+// PARA INICIAR SESION
+router.use(bodyParser.json()); //para soportar JSON encoded bodies
+router.use(bodyParser.urlencoded({
+    extended: true}));
+router.use(bodyParser.urlencoded({extended:true}));
+router.use(cookieParser('secreto'));
+router.use(session({
+    secret: 'secreto',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Configuracion Passport //
+router.use(passport.initialize());
+router.use(passport.session());
 
 
+passport.use(new PassportLocal(function(username,password,done){
+    // done(err,{name: "uriel"},) usa 3 argumentos, name define si el inicio se inicio correctamente
+    if (username === "codigofacilito" && password === "1234")
+    return done(null,{id: 1 , name:"cody"});
 
-router.get ('/', (req,res) => { // al usar routes, los get ya no son app.use sino router.get LEER SOBRE MODULES DE EXPRESS Y LOS MIDDLE WORD
-    res.render('index');
+    done(null,false);
+}));
+//{id: 1 , name:"cody"})
+//1 => serializacion pasarle todo el objeto a un dato, cuando necesito usar al usario tomo el id y retorno el objeto (deserializacion)
+passport.serializeUser(function(user,done){
+    done(null,user.id);
 })
 
-export default router //exportar todo lo que está en routes.js
+passport.deserializeUser(function(id,done){
+   done(null,{id: 1 , name:"cody"}) ;
+})
+
+
+
+router.get("/",(req,res)=>{
+    // si ya inciamos mostar bienvenida
+    res.render("home")
+    // si no hemos iniciado sesion redireccionar a /login
+})
+
+router.get("/arquitectura",(req,res)=>{
+    // si ya inciamos mostar bienvenida
+    res.render("arquitectura")
+    // si no hemos iniciado sesion redireccionar a /login
+})
+
+router.get("/bootcamp",(req,res)=>{
+    // si ya inciamos mostar bienvenida
+    res.render("bootcamp")
+    // si no hemos iniciado sesion redireccionar a /login
+})
+
+
+router.get("/app",(req,res)=>{
+    //mostar el formulario de login
+    res.render("app")
+    
+    
+})
+
+
+router.get("/inicio",(req,res)=>{
+    //mostar el formulario de login
+    res.render("inicio")
+    
+    
+})
+router.get("/inicio",(req,res)=>{
+    //mostar el formulario de login
+    res.render("inicio")
+
+})
+
+router.post("/inicio", passport.authenticate('local',{
+    successRedirect: "app",
+    failureRedirect: "inicio",
+
+}))
+
+router.get("/fondo", (req,res)=>{
+    res.render("fondo")
+
+})
+
+
+
+
+// router.get ('/', (req,res) => { // al usar routes, los get ya no son app.use sino router.get LEER SOBRE MODULES DE EXPRESS Y LOS MIDDLE WORD
+//     res.render('index');
+// })
+
+export default router //exportar todo lo que está en routes.jss
